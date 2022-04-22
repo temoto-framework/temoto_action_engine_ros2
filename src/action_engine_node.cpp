a06196b3778bfc9abb9e648f24eed6e43e20d85f
Robert <valnerrobert@gmail.com>
2022-04-22 19:19:13 +0300

    Created a base ROS2 package structure

---
+++ src/action_engine_node.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include "temoto_action_engine/action_engine.h"
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world temoto_action_engine_ros2 package\n");
+  return 0;
+}
